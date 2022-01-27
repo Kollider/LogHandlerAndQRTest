f6ded75ed5ad2ccb2799c9f2466599350e5c1a0f
Andrii <kollider184@gmail.com>
2022-01-27 21:12:54 +0200

    Almost

--- LogReader.py
+++ LogReader.py
@@ -2,73 +2,64 @@
 # 2021-11-15 13:36:11,423 - DEBUG - Detect ec54a8
 # 2021-11-15 13:36:11,913 - DEBUG - > 'BIG;77;D5FA3F;1;66;42;6160;1;2;46;5;0;0;141;1;-3474;1;DD;'
 # 2021-11-15 13:36:11,913 - WARNING - > 'BAD;75;ec803d;8450;1;02;'
-import json
 
-working_ids = []
-notWorking_ids = []
-
+#
 data = []
-order_1 = ["handler", 'numb1', "id", 'numb2', 'numb3', 'numb4', 'numb5', 'numb6', 'numb7', 'numb8', 'numb9', 'numb10',
-           'numb11', 'numb12', 'numb13', 'numb14', 'numb15', 'state']
+order = ["handler", 'numb1', "id", 'numb2', 'numb3', 'numb4', 'numb5', 'numb6', 'numb7', 'numb8', 'numb9', 'numb10',
+         'numb11', 'numb12', 'numb13', 'numb14', 'numb15', 'state']
 
-order_2 = ["handler", 'numb1', "id", 'numb5', 'numb16', 'state']
-
-with open("app_2.log") as f:
-    file_for_logs = open("demofile2.txt", "w")
-    count = 0
-    new_count = 0
-    for line in f:
-        marker_left = line.find("'")
-        marker_right = line.rfind("'")
-        if marker_right and marker_left != -1:
-            line_without_date = line[marker_left + 1:marker_right - 1]
-            content = line_without_date.split(";")
-            if len(content) > 6:
-                content = [x.strip() for x in content]
-                structure = {key: value for key, value in zip(order_1, content)}
-                data.append(structure)
-                new_line = 1
-                # print(marker_left, marker_right)
-                new_count += 1
-        count += 1
-    """    print(count)
-    print(new_count)
-    print(notWorking_ids)"""
-"""for entry in data:
-    file_for_logs.write(json.dumps(entry, indent=4) + '\n')"""
-
-devices = {'92ABF9': 1}
+devices = {}
 not_working_devices = []
 
-for item in data:
-    print(item['id'], item['state'])
-    dev_id = item['id']
-    dev_state = item['state']
 
-    if dev_id in devices and dev_state != 'DD':
-        dev_number = devices[dev_id]
-        print(dev_number)
-        new_dev_number = dev_number + 1
-        devices[dev_id] = new_dev_number
+def log_to_dictionary(filename):
+    with open(filename) as f:
+        for line in f:
+            marker_left = line.find("'")
+            marker_right = line.rfind("'")
+            if marker_right != -1:
+                line_without_date = line[marker_left + 1:marker_right - 1]
+                content = line_without_date.split(";")
+                content = [x.strip() for x in content]
+                if len(content) > 6:
+                    structure = {key: value for key, value in zip(order, content)}
+                    data.append(structure)
 
 
-    else:
-        if dev_state == 'DD':
-            devices.pop(dev_id, None)
-            if dev_id not in not_working_devices:
-                not_working_devices.append(dev_id)
-        elif dev_state == '02':
-            if dev_id in not_working_devices:
-                pass
-            else:
-                devices[dev_id]=1
+def dict_filter(dictionary):
+    for item in dictionary:
+        dev_id = item['id']
+        dev_state = item['state']
 
-    file_for_logs.write(item['state'] + '\n')
+        if dev_id in devices and dev_state != 'DD':
+            dev_number = devices[dev_id]
+            new_dev_number = dev_number + 1
+            devices[dev_id] = new_dev_number
 
-    """for key in item:
-        print('key =',key,'\n', 'value=',item[key])"""
-file_for_logs.close()
+        else:
+            if dev_state == 'DD':
+                devices.pop(dev_id, None)
+                if dev_id not in not_working_devices:
+                    not_working_devices.append(dev_id)
+            elif dev_state == '02':
+                if dev_id in not_working_devices:
+                    pass
+                else:
+                    devices[dev_id] = 1
 
-print('devices = ',len(devices))
 
-print(not_working_devices)
+
+log_to_dictionary("app_2.log")
+dict_filter(data)
+
+work_devices =len(devices)
+notwork_devices = len(not_working_devices)
+
+print(f'______Failed test {notwork_devices} devices______')
+for device in not_working_devices:
+    print(f'Device: {device} was removed')
+
+print(f'______Success test {work_devices} devices______')
+for device in devices:
+    #print(device,devices[device])
+    print(f'Device {device} sent {devices[device]} statuses')
